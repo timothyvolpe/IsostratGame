beb08712202b19bf7acb7c5c4014f357d32c7aca
tvolpes <timothybvolpe@gmail.com>
2015-12-08 20:15:51 -0500

    Added output to a text file

--- IsostratGame/include/console.h
+++ IsostratGame/include/console.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <fstream>
+
 #include "def.h"
 #include "platform.h"
 
@@ -12,9 +14,9 @@ enum : unsigned char
 
 // Variadic console output macros
 #if defined( _WINDOWS ) && defined( EXTERNAL_DEBUG_CONSOLE )
-#define PrintInfo(msg, ...) CGame::instance().getConsole()->printColor(L"", CONSOLE_COLOR_DEFAULT); wprintf(msg, ##__VA_ARGS__ )
-#define PrintWarn(msg, ...) CGame::instance().getConsole()->printColor(L"WARN: ", CONSOLE_COLOR_WARNING); wprintf(msg, ##__VA_ARGS__ )
-#define PrintError(msg, ...) CGame::instance().getConsole()->printColor(L"ERROR: ", CONSOLE_COLOR_ERROR); wprintf(msg, ##__VA_ARGS__ )
+#define PrintInfo(msg, ...) CGame::instance().getConsole()->printColor(L"", CONSOLE_COLOR_DEFAULT); wprintf(msg, ##__VA_ARGS__ ); fwprintf( CGame::instance().getConsole()->getLogFile(), msg, ##__VA_ARGS__ )
+#define PrintWarn(msg, ...) CGame::instance().getConsole()->printColor(L"WARN: ", CONSOLE_COLOR_WARNING); wprintf(msg, ##__VA_ARGS__ ); fwprintf( CGame::instance().getConsole()->getLogFile(), msg, ##__VA_ARGS__ )
+#define PrintError(msg, ...) CGame::instance().getConsole()->printColor(L"ERROR: ", CONSOLE_COLOR_ERROR); wprintf(msg, ##__VA_ARGS__ ); fwprintf( CGame::instance().getConsole()->getLogFile(), msg, ##__VA_ARGS__ )
 #else
 #define PrintInfo(msg, ...) 
 #define PrintWarn(msg, ...)
@@ -26,7 +28,10 @@ class CConsole
 private:
 #ifdef _WINDOWS
 	FILE *m_hCout, *m_hCerr;
+	FILE *m_hLogFile;
 #endif
+	bool m_bFileLogging;
+
 public:
 	CConsole();
 	~CConsole();
@@ -37,4 +42,6 @@ public:
 	void setColor( unsigned char color );
 
 	void printColor( std::wstring message, unsigned char color );
+
+	FILE* getLogFile();
 };
--- IsostratGame/source/console.cpp
+++ IsostratGame/source/console.cpp
@@ -4,6 +4,7 @@
 #include <io.h>
 #include <fcntl.h>
 #endif
+#include <boost\filesystem.hpp>
 
 #include "console.h"
 
@@ -11,12 +12,16 @@
 CConsole::CConsole() {
 	m_hCout = NULL;
 	m_hCerr = NULL;
+	m_hLogFile = NULL;
+	m_bFileLogging = false;
 }
 CConsole::~CConsole() {
 }
 
 bool CConsole::initialize()
 {
+	boost::filesystem::path logPath;
+
 #ifdef EXTERNAL_DEBUG_CONSOLE
 	// Allocate the console
 	AllocConsole();
@@ -25,10 +30,29 @@ bool CConsole::initialize()
 	freopen_s( &m_hCerr, "CONOUT$", "w", stderr );
 #endif
 
+	// Attempt to open the log file
+	logPath = boost::filesystem::current_path();
+	logPath /= FILESYSTEM_LOGDIR;
+	// Create log dir if needed
+	if( !boost::filesystem::is_directory( logPath ) )
+		boost::filesystem::create_directory( logPath );
+	logPath /= "console.log";
+	// Open the file
+	fopen_s( &m_hLogFile, logPath.string().c_str(), "w" );
+	if( !m_hLogFile )
+		m_bFileLogging = false;
+	else
+		m_bFileLogging = true;
+
 	return true;
 }
 void CConsole::destroy()
 {
+	if( m_hLogFile ) {
+		fclose( m_hLogFile );
+		m_hLogFile = NULL;
+	}
+
 #ifdef EXTERNAL_DEBUG_CONSOLE
 	// Destroy the out handle
 	if( m_hCout ) {
@@ -72,4 +96,9 @@ void CConsole::printColor( std::wstring message, unsigned char color )
 #ifdef EXTERNAL_DEBUG_CONSOLE
 	std::wcout << message.c_str();
 #endif
+	if( m_bFileLogging && m_hLogFile )
+		fwprintf( m_hLogFile, message.c_str() );
+}
+FILE* CConsole::getLogFile() {
+	return m_hLogFile;
 }
