f61adc45e4d59ddd95adf52bcdba27b18b441a7e
tvolpes <timothybvolpe@gmail.com>
2015-12-10 20:39:34 -0500

    Changed to opengl 3.3....

--- IsostratGame/include/def.h
+++ IsostratGame/include/def.h
@@ -8,7 +8,7 @@
 #define WINDOW_DEFRES_Y 600
 
 #define GL_VERSION_MAJOR 3
-#define GL_VERSION_MINOR 2
+#define GL_VERSION_MINOR 3
 
 #define FILESYSTEM_SHADERDIR L"data\\shaders"
 #define FILESYSTEM_LOGDIR L"logs"
